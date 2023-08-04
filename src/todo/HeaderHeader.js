0e5bc7387bf24450e56c0206d78bcb1755a87032
prince475 <danielmwai475@gmail.com>
2023-08-04 05:27:25 +0300

    Main heading of the my recipe.

---
+++ src/todo/HeaderHeader.js
@@ -0,0 +1,41 @@
+import React from "react";
+import AddButton from "./AddButton";
+import AddRecipe from "./AddRecipe";
+import '../styles/layout.css'
+
+
+class HeaderHeader extends React.Component {
+	constructor() {
+	  super();
+	  this.state = {
+		isHidden: true
+	  };
+	}
+
+	toggleHidden() {
+	  if (this.state.isHidden) {
+		this.setState({ isHidden: false });
+	  } else {
+		this.setState({ isHidden: true });
+	  }
+	}
+
+	render() {
+	  return (
+		<header className="page__header">
+		  <div className="container flex-container">
+			<h1>My Recipes</h1>
+			<AddButton toggleHidden={this.toggleHidden.bind(this)} />
+		  </div>
+		  {!this.state.isHidden && (
+			<AddRecipe
+			  add={this.props.add}
+			  toggleHidden={this.toggleHidden.bind(this)}
+			/>
+		  )}
+		</header>
+	  );
+	}
+  }
+
+export default HeaderHeader;
