a142aab4b3ecbd8a3ba48cbbef508e9b3977f28b
prince475 <danielmwai475@gmail.com>
2023-08-04 05:21:51 +0300

    Creating our footer box layout.

---
+++ src/todo/BoxFooter.js
@@ -0,0 +1,19 @@
+import React from "react";
+import '../styles/layout.css'
+
+
+class BoxFooter extends React.Component {
+	render() {
+	  return (
+		<footer className="box__footer">
+		  <button className="delete-btn-box" onClick={this.props.delete} >Delete
+		  </button>
+		  <button onClick={this.props.toggleEdit} className="edit-btn-box">
+				 Edit
+		  </button>
+		</footer>
+	  );
+	}
+  }
+
+export default BoxFooter
